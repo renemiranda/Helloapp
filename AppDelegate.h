2b4a05d3bdb0611b4fad712bc39962a1f9cdbd85
Rene Miranda <rmiran8@tigers.lsu.edu>
2012-07-31 13:57:51 -0500

    ADD

---
+++ AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Helloapp
+//
+//  Created by RENE MIRANDA on 7/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Helloapp
+//
+//  Created by RENE MIRANDA on 7/31/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
